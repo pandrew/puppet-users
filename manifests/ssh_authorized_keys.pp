03523f246eb39d4281077131b9d3d76f22d23a12
Paul Andrew Liljenberg <letters@paulnotcom.se>
2014-04-24 15:14:08 +0200

    point to user

--- manifests/ssh_authorized_keys.pp
+++ manifests/ssh_authorized_keys.pp
@@ -1,7 +1,7 @@
 define users::ssh_authorized_keys($user, $hash, $h_key) {
   if(!defined(Ssh_authorized_keys[$user])) {
     ssh_authorized_key { "${user}-${h_key}" :
-      ensure   => $hash["$h_key"]['ensure'],
+      ensure   => $hash["$user"]['ensure'],
       key      => $hash["$h_key"]['key'],
       options  => $hash["$h_key"]['options'],
       provider => $hash["$h_key"]['provider'],
